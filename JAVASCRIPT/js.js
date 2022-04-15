f7a3d1bb56957bd037e06a62832d917ad599c57e
MrLeonWeekes <48031756+MrLeonWeekes@users.noreply.github.com>
2022-04-15 20:48:25 +0100

    sdds

--- JAVASCRIPT/js.js
+++ JAVASCRIPT/js.js
@@ -69,57 +69,57 @@ window.addEventListener('load', () => {
 // ------------------------------- FLAPPY BIRD
 // -------------------------------------------
 
-var block = document.getElementById("block");
-var hole = document.getElementById("hole");
-var character = document.getElementById("character");
-var jumping = 0;
-var counter = 0;
-var highscore = [];
-hole.addEventListener('animationiteration', () => {
-    var random = -((Math.random() * 300) + 150);
-    hole.style.top = random + "px";
-    counter++;
-});
+// var block = document.getElementById("block");
+// var hole = document.getElementById("hole");
+// var character = document.getElementById("character");
+// var jumping = 0;
+// var counter = 0;
+// var highscore = [];
+// hole.addEventListener('animationiteration', () => {
+//     var random = -((Math.random() * 300) + 150);
+//     hole.style.top = random + "px";
+//     counter++;
+// });
 
 
 
-const btnStart = document.getElementById("character");
+// const btnStart = document.getElementById("character");
 
-btnStart.addEventListener("click", function () {
+// btnStart.addEventListener("click", function () {
 
-    const sss = setInterval(function start() {
-        var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-        if (jumping == 0) {
-            character.style.top = (characterTop + 3) + "px";
-        }
-        var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-        var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
-        var cTop = -(650 - characterTop);
-        if ((characterTop > 630) || ((blockLeft < 20) && (blockLeft > -50) && ((cTop < holeTop) || (cTop > holeTop + 280)))) {
-            clearInterval(sss);
-            document.getElementById("high-score").innerHTML = counter;
-            character.style.top = 350 + "px";
-            counter = 0;
-        }
-    }, 10);
-});
+//     const sss = setInterval(function start() {
+//         var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+//         if (jumping == 0) {
+//             character.style.top = (characterTop + 3) + "px";
+//         }
+//         var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+//         var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+//         var cTop = -(650 - characterTop);
+//         if ((characterTop > 630) || ((blockLeft < 20) && (blockLeft > -50) && ((cTop < holeTop) || (cTop > holeTop + 280)))) {
+//             clearInterval(sss);
+//             document.getElementById("high-score").innerHTML = counter;
+//             character.style.top = 350 + "px";
+//             counter = 0;
+//         }
+//     }, 10);
+// });
 
-function jump() {
-    jumping = 1;
-    let jumpCount = 0;
-    var jumpInterval = setInterval(function () {
-        var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-        if ((characterTop > 6) && (jumpCount < 15)) {
-            character.style.top = (characterTop - 5) + "px";
-        }
-        if (jumpCount > 20) {
-            clearInterval(jumpInterval);
-            jumping = 0;
-            jumpCount = 0;
-        }
-        jumpCount++;
-    }, 10);
-};
+// function jump() {
+//     jumping = 1;
+//     let jumpCount = 0;
+//     var jumpInterval = setInterval(function () {
+//         var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+//         if ((characterTop > 6) && (jumpCount < 15)) {
+//             character.style.top = (characterTop - 5) + "px";
+//         }
+//         if (jumpCount > 20) {
+//             clearInterval(jumpInterval);
+//             jumping = 0;
+//             jumpCount = 0;
+//         }
+//         jumpCount++;
+//     }, 10);
+// };
 
 
 
@@ -132,32 +132,21 @@ const api = {
     base: "https://api.openweathermap.org/data/2.5/"
 }
 
-
-// SET UP AN EVENT LISTENER FOR WHEN THE ENTER KEY IS PRESSED 
-// ONCE PRESSED IT WILL RUN THE "SETQUERY" FUNCTION
 const searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress', setQuery);
 
-// THIS FUNCTION GETS THE ENTER DETAILS FRON THE INPUT BOX
 function setQuery(evt) {
     if (evt.keyCode == 13) {
         getResults(searchbox.value)
     }
 }
 
-// THIS FUNCTION RUNS A FETCH REQUEST AND BUILD UP THE URL THAT HAS ALL THE DETAILS TO OBTAIN THE CORRECT INFO FROM THE WEATHER WEBSITE
-// THE BASE URL THEN THEN STANDARD TEXT ALONG WITH THE QUERY WHICH IS "LONDON" FOR EXAMPLE THEN MNORE STANDARD TEXT AND FINALLY THE API KEY
-// "&units=metric" CONVERT TO CELCIUS
-// .THEN DEALS WITH API CALLS
-// CONVERTS IT TO WEATHER VARIABLE AND CONVERTS IT TO JSON THEN PASS THE JSON THROUGH "displayResults" NAMED AS WEATHER
 function getResults(query) {
     fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
         .then(weather => {
             return weather.json();
         }).then(displayResults);
 }
-
-// 
 
 function displayResults(weather) {
     console.log(weather)
@@ -193,8 +182,6 @@ function displayResults(weather) {
         document.querySelector('.game4').style.color = "#fff";
         // document.querySelector('.text-color').style.color = "rgb(0, 128, 55)";
     }
-
-
 }
 
 function dateBuilder(d) {
