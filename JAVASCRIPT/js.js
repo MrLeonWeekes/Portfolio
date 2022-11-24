eab09f2447f717eadb528c6688ee4dd59a5c1e45
leonweekes <leon.weekes@aol.co.uk>
2022-11-24 20:25:12 +0000

    weather app

--- JAVASCRIPT/js.js
+++ JAVASCRIPT/js.js
@@ -186,8 +186,8 @@ function displayResults(weather) {
 
 function dateBuilder(d) {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    let day = days[d.getDay()];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let day = days[d.getDay() - 0];
     let date = d.getDate();
     let month = months[d.getMonth()]
     let year = d.getFullYear();
